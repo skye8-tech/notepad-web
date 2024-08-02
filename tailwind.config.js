5cd9a3f327931e640c3188ec9f571f25cb498e4e
Ngochi-N <ngotsohngochi@gmail.com>
2024-08-02 14:01:31 +0100

    started page/Dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3471FF",
+        light: {
+          800: "#AFAFAF"
+        },
+        dark: {
+          600: "#484848",
+          700: "#343434",
+          800: "#272727",
+          900: "#1B1B1B",
+        }
+      }
+    },
   },
   plugins: [],
 };
